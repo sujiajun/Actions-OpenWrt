77a966392de1d9d8f521fef01d43c2c44f8a4b3d
sujiajun <dennycai@qq.com>
2022-04-18 15:34:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,19 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git packages
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-rm -rf package/lean/luci-theme-argon
-mkdir -p package/myapps
-cd package/myapps
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/kenzok8/openwrt-packages.git
-# git clone https://github.com/Ausaci/luci-app-nat6-helper.git
-git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone https://github.com/ntlf9t/luci-app-easymesh.git
-git clone https://github.com/destan19/OpenAppFilter.git
