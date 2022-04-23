8af300ea94f0750c19f4fa15d60ac51cfc434b2e
sujiajun <dennycai@qq.com>
2022-04-23 09:57:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,3 @@ sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.d
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git neobird https://github.com/thinktip/luci-theme-neobird.git' >>feeds.conf.default
-
-git clone https://github.com/kenzok78/luci-theme-argonne.git
