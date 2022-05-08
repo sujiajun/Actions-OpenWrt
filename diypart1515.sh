58f2400aed6b1acf3b2de38131537fdd01d95fa8
sujiajun <dennycai@qq.com>
2022-05-08 19:37:24 +0800

    Update diypart1515.sh

--- diypart1515.sh
+++ diypart1515.sh
@@ -22,3 +22,4 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2.git
+git clone https://github.com/kenzok8/small-package/luci-theme-argon.git
